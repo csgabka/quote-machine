3a515bcae6068b6bec6b929aa735cf7ed6ca223e
Gabriella <csgabka@users.noreply.github.com>
2018-12-18 21:00:39 +0000

    twitter

--- src/components/share.js
+++ src/components/share.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import SVG from './img/twitter.svg'
 
 const share = ({quoteText, quoteAuthor}) => {
 		return(
 			<div>
 			<a class="twitter-share-button" id="tweet" href={"https://twitter.com/intent/tweet?text="
-			 + quoteText + " - " + quoteAuthor}>Tweet</a>
+			 + quoteText + " - " + quoteAuthor}><img id="twitter" src={SVG}/></a>
 			</div>
 			);
 }
