4bf7d77c74d7e4cba8482edc1b7699264f1ae44b
Gabriella <csgabka@users.noreply.github.com>
2018-12-18 17:21:22 +0000

    Updates

---
+++ precache-manifest.2e13fc8e64f14765e9d517963a5c0c92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "abe5ed556fd329af181a4b9c6a140eb1",
+    "url": "/quote-machine/static/media/bg.abe5ed55.jpg"
+  },
+  {
+    "revision": "22742a60cdb878f6f135",
+    "url": "/quote-machine/static/js/runtime~main.22742a60.js"
+  },
+  {
+    "revision": "2af3a32746b7c165035b",
+    "url": "/quote-machine/static/js/main.2af3a327.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/quote-machine/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "2af3a32746b7c165035b",
+    "url": "/quote-machine/static/css/main.1d682fcc.chunk.css"
+  },
+  {
+    "revision": "39529ded99bf4eedf58017ac77f62cf1",
+    "url": "/quote-machine/index.html"
+  }
+];
